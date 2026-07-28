74814429c3b8a5a23286f10fd4e08ee0a6e6cb8f
Tunamaran <tunamaran@users.noreply.github.com>
2026-07-28 17:58:48 +0300

    fix(i18n): add missing using ITAD.LibrarySync.Core.Launchers directive to LanguageManager

--- src/ITAD.LibrarySync.App/Services/LanguageManager.cs
+++ src/ITAD.LibrarySync.App/Services/LanguageManager.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using ITAD.LibrarySync.Core.Launchers;
 using ITAD.LibrarySync.Core.Scheduling;
 
 namespace ITAD.LibrarySync.App.Services;
